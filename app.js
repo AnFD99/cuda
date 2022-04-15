3733635edf5397ff30e1010a39c32dde4fc3ff9e
Anastasia Fadeeva <anfd.design@gmail.com>
2022-04-15 18:55:01 +0200

    new

--- app.js
+++ app.js
@@ -1,26 +1,22 @@
-
 // Burger
 const burger = document.querySelector('#burgerMenu')
 const nav = document.querySelector('#nav')
-   
-   
-burger.addEventListener('click', event => {
+
+burger.addEventListener('click', (event) => {
    event.preventDefault()
-   
+
    nav.classList.toggle('show')
 })
-   
-   
+
 // Filter
 const buttons = document.querySelectorAll('.portfolio-btn')
 const cards = document.querySelectorAll('.portfolio-item')
 
-
 function filter(category, items) {
-   items.forEach(item => {
+   items.forEach((item) => {
       const isItemFiltered = !item.classList.contains(category)
       const isShowAll = category === 'all'
-      if(isItemFiltered && !isShowAll) {
+      if (isItemFiltered && !isShowAll) {
          item.classList.add('hide')
       } else {
          item.classList.remove('hide')
@@ -28,65 +24,35 @@ function filter(category, items) {
    })
 }
 
-buttons.forEach(button => {
-   button.addEventListener('click', event => {
+buttons.forEach((button) => {
+   button.addEventListener('click', (event) => {
       event.preventDefault()
       const currentCategory = button.dataset.filter
       filter(currentCategory, cards)
    })
 })
 
-
 // Form
-let form = document.querySelector('#form')
-
-form.addEventListener('submit', formSubmit)
-
-async function formSubmit(event) {
-   event.preventDefault()
-   const data = serializeForm(event.target)
-   const {status, error} = await sendData(data)
-
-   if (status === 200) {
-      onSuccess(event.target)
-   } else {
-      onError(error)
-   }
-}
-
-function serializeForm(formNode) {
-      const data = new FormData(formNode)
-      console.log(Array.from(data.entries()))
-      return data
-}
-
-
-async function sendData(data) {
-   // return await fetch('/api/apply/', {
-   //    method: 'POST',
-   //    headers: { 'Content-Type': 'multipart/form-data'},
-   //    body: data,
-   // })
-
-   return new Promise(resolve => {
-      setTimeout(() => {
-         resolve({
-            status: 200,
-            error: {
-               message: 'Что-то пошло не так!'
-            }
-         })
+document.addEventListener('DOMContentLoaded', () => {
+   function ajaxSend(formData) {
+      return fetch('https://jsonplaceholder.typicode.com/posts', {
+         method: 'POST',
+         headers: { 'Content-Type': 'aplication/json' },
+         body: JSON.stringify(formData),
       })
-   })
-}
+         .then((formData) => console.log(formData))
+         .catch((error) => console.error(error))
+   }
 
-function onSuccess(formNode) {
-   alert('Ваша заявка успешно отправлена!')
- }
+   const form = document.querySelector('#form')
+   form.addEventListener('submit', formSubmit)
 
- function onError(error) {
-   alert(error.message)
- }
+   function formSubmit(event) {
+      event.preventDefault()
 
+      let formData = new FormData(this)
+      formData = Object.fromEntries(formData)
 
-
+      ajaxSend(formData)
+   }
+})
